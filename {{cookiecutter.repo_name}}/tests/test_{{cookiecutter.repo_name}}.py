91a7cb5ede4c24852077ec156106b0b2d8c4e65a
Bystroushaak <bystrousak@kitakitsune.org>
2014-12-27 00:42:04 +0100

    Added test file.

---
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import sys
+
+
+import pytest
+
+sys.path.append('src/')
+import {{cookiecutter.repo_name}}
+
+
+# Variables ===================================================================
+
+
+
+# Functions & classes =========================================================
+def test_{{cookiecutter.repo_name}}():
+    pass
